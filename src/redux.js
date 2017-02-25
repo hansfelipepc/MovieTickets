9acaffb165fa478fc31e46c017206b11501b161a
Hans Felipe <hansfelipepc@outlook.com>
2017-02-25 00:19:07 -0300

    Updated redux file

--- src/redux.js
+++ src/redux.js
@@ -25,3 +25,22 @@ export const apiMiddleware = store => next => action => {
       break;
   }
 };
+
+export const reducer = (state = { movies: [], loading: true }, action) => {
+  switch (action.type) {
+    case 'GET_MOVIE_DATA_LOADING':
+      return {
+        ...state,                   // keep the existing state,
+        loading: true,              // but change loading to true
+      };
+    case 'GET_MOVIE_DATA_RECEIVED':
+      return {
+        loading: false,             // set loading to false
+        movies: action.data.movies, // update movies array with reponse data
+      };
+    case 'GET_MOVIE_DATA_ERROR':
+      return state;
+    default:
+      return state;
+  }
+};
